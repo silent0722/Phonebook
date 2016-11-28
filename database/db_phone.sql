ab697c7e97e9be004bb2fd72e2069658393b34b0
silent0722 <averilla.amiel22@yahoo.com>
2016-11-28 14:52:05 +0800

    Add files via upload
    Good day sir ! I've just edited your previous phonebook webpage. I added some login page for the user and contacts and I also created a database for that. Same as the interface. I hope this will help you.

---
+++ database/db_phone.sql
@@ -0,0 +1,36 @@
+-- phpMyAdmin SQL Dump
+-- version 4.0.9
+-- http://www.phpmyadmin.net
+--
+-- Host: 127.0.0.1
+-- Generation Time: Nov 25, 2016 at 05:55 AM
+-- Server version: 5.5.34
+-- PHP Version: 5.4.22
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `db_phone`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_des`
+--
+
+CREATE TABLE IF NOT EXISTS `tbl_des` (
+  `Nname` varchar(50) NOT NULL,
+  `Pword` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
